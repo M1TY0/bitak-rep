8d1e673616658e6946f4de6f60cdbfa12677bb5d
M1TY0 <100305309+M1TY0@users.noreply.github.com>
2023-09-07 16:01:33 +0300

    3 new api functions

---
+++ bitak-api/bitak-api/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Principal;
+
+namespace bitak_api.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class User : ControllerBase
+    {
+        [HttpGet]
+        public void createUser(int id, string username, string password, string email)
+        {
+            string connection = "SERVER = localhost; database = bitakdb; uid = root; password= 1234;";
+            MySqlConnection conn = new MySqlConnection(connection);
+            conn.Open();
+            string query1 = "INSERT INTO users(id,username,password,email,date) VALUES(" + id + ",'" + username + "','" + password + "','" + email + "',NOW())";
+            MySqlCommand cmd1 = new MySqlCommand(query1, conn);
+            cmd1.ExecuteScalar();
+        }
+    }
+    public class Password : Controller
+    {
+        
+        [Route("[controller]")]
+        [HttpGet]
+        public bool checkPassword(string email)
+        {
+            string connection = "SERVER = localhost; database = bitakdb; uid = root; password= 1234;";
+            MySqlConnection conn = new MySqlConnection(connection);
+            conn.Open();
+            string query1 = "SELECT * FROM users WHERE email = '" + email + "';";
+            MySqlCommand cmd1 = new MySqlCommand(query1, conn);
+            try
+            {
+                cmd1.ExecuteScalar().ToString();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    
+}
+       
+}
